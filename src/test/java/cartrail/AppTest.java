33aef2004af7943e98871d5f3a438afcbe61669f
soul0328 <278834379@qq.com>
2015-02-02 10:26:28 +0800

    加一些文件，以致可以上传文件夹

---
+++ src/test/java/cartrail/AppTest.java
@@ -0,0 +1,25 @@
+/**
+ * 项目名：cartrail
+ * 包名：cartrail
+ * 文件名：AppTest.java
+ * 版本信息：@version 1.0
+ * 日期：2015年2月2日-上午10:20:33
+ * Copyright 2015 joymain Tech. Co. Ltd. All Rights Reserved.  
+ */
+package cartrail;
+
+/**
+ * 
+ * 
+ * @author soul
+ * @email soul.lau0328@gmail.com
+ * @qq 278834379
+ * @since 2015年2月2日 上午10:20:33
+ *
+ */
+public class AppTest {
+
+    public static void main(String args[]) {
+        System.out.println("cartrail");
+    }
+}
